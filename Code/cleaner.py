d86050bd5a51c2fa7c4c7372a3e058bc87978e7f
TyJK <TJKolody@gmail.com>
2017-09-27 14:11:22 -0400

    Update cleaner.py

--- Code/cleaner.py
+++ Code/cleaner.py
@@ -14,14 +14,14 @@ with open("Clean.xml", "w", encoding='utf8') as x:
         element = element.lower()
         # Uncomment to strip out tags as well, to recover raw text
         # element = re.sub(r'<.*?>', '', element)
-        element = re.sub(r'https?:\/\/.*[]', 'removedurl', element)
+        element = re.sub(r'https?:\/\/.*[]', ' removedurl ', element)
         newElement = []
         # Used to anonymize email addresses and user targets (already done in base datasets)
         for word in element.split(" "):
             if "@" in word and ".com" in word or "gmail" in word or "yahoo" in word or ".fr" in word or ".ca" in word:
-                word = "redactedemail"
+                word = " redactedemail "
             elif word.startswith("@"):
-                word = "redactedusername"
+                word = " redactedusername "
             newElement.append(word)
         element = " ".join(newElement)
         # completely strips everything that is not in the french alphebet, a number, a tag, newline or space
