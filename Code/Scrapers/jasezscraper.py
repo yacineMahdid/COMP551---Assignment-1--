e5263128616d9229348a27210c19a8b038ea6ab4
TyJK <TJKolody@gmail.com>
2017-09-27 23:00:23 -0400

    Update jasezscraper.py
    Added comments, cleaned code

--- Code/Scrapers/jasezscraper.py
+++ Code/Scrapers/jasezscraper.py
@@ -30,12 +30,7 @@ def nextPage(webPage, tag, attr):
 punctuation = "><\n\r"
 replace = "    "
 transtable = str.maketrans(punctuation, replace)
-# Things to do
-# monitoring system for thread collection and comment collection
-# xml formatting
-# conditional parsing so it doesn't take threads that are too short
-# figure out what to do about quotations and other repetitive strings
-# what sort of cleaning are we going to do
+
 
 #
 # firstSite = {'startLink': 'http://www.jasez.ca/forums',
@@ -46,30 +41,30 @@ transtable = str.maketrans(punctuation, replace)
 #              'content': ['td', {'class': 'section'}]}
 
 
-# directory = []
-# startURL = "http://www.jasez.ca/forums"
-# linkScrape(startURL, 'td', {'class': "desc"}, directory)
-# print(directory)
+directory = []
+startURL = "http://www.jasez.ca/forums"
+linkScrape(startURL, 'td', {'class': "desc"}, directory)
+print(directory)
 
-
-# count = 0
-# for page in directory:
-#     fileDump = []
-#     while len(page) >= len(directory[count]):
-#         linkScrape(page, 'h3', {'class': "topic"}, fileDump)
-#         page = nextPage(page, 'div', {'class': "right"})
-#         print(page)
-#     print(fileDump)
-#     with open("jasezlinks3.txt", "a", encoding='utf8') as x:
-#         for element in set(fileDump):
-#             x.write(element + '\n')
-#     count += 1
+count = 0
+for page in directory:
+    fileDump = []
+    while len(page) >= len(directory[count]):
+        linkScrape(page, 'h3', {'class': "topic"}, fileDump)
+        page = nextPage(page, 'div', {'class': "right"})
+        print(page)
+    print(fileDump)
+    with open("jasezlinks3.txt", "a", encoding='utf8') as x:
+        for element in set(fileDump):
+            x.write(element + '\n')
+    count += 1
 
 with open("jasezlinks.txt", 'r', encoding='utf8') as urlDirectory:
     for url in urlDirectory:
         print(url)
-        with open('jasezx.xml', 'a', encoding='utf8') as file:
+        with open('jasez.xml', 'a', encoding='utf8') as file:
             file.write("<s>")
+            # Jasez sometimes sends you back to the home page, and sometimes returns nothing when the end of thread is reached
             while url != "http://www.jasez.ca/forums/" and url is not None:
                 site = requests.get(url)
                 soup = BeautifulSoup(site.text, "lxml")
@@ -85,6 +80,7 @@ with open("jasezlinks.txt", 'r', encoding='utf8') as urlDirectory:
                     for post in soup.find_all('tbody', attrs={'class': "post"}):
                         speaker = re.search(r'/profil/[^"]*', str(post()))[0]
                         speaker = re.sub(r'/profil/', '', speaker)
+                        # Sets the uid so that anyone new gets added, and anyone responding again gets the same ID as before
                         if speaker in uidList:
                             speaker = uidList.index(speaker) + 1
                         else:
@@ -92,8 +88,10 @@ with open("jasezlinks.txt", 'r', encoding='utf8') as urlDirectory:
                             speaker = uidList.index(speaker) + 1
                         rawText = str(post.find('td', attrs={'class': 'section'}))
                         rawText = re.sub(r'.*?</span>', '', rawText)
+                        # Eliminates all html tags or tag fragments as well as newlines to keep things neat
                         rawText = re.sub(r'<.*?>', '', rawText)
                         rawText = re.sub(r'<|>|\r|\n', '', rawText)
+                        # catches any extranious newlines and tags
                         rawText = rawText.translate(transtable)
                         cleanText = ftfy.fix_text(rawText)
                         file.write('<utt uid={}>{}</utt>'.format(speaker, cleanText))
